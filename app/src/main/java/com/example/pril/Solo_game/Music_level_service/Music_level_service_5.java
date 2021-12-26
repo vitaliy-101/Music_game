5b7c71180994d23073abfd52b7f180eb04d98cac
vitaliy-101 <vitaliy10kostin2004@mail.ru>
2021-12-26 16:48:53 +0300

    yes

---
+++ app/src/main/java/com/example/pril/Solo_game/Music_level_service/Music_level_service_5.java
@@ -0,0 +1,45 @@
+package com.example.pril.Solo_game.Music_level_service;
+
+import android.app.IntentService;
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.renderscript.ScriptGroup;
+import android.text.Layout;
+import android.widget.Toast;
+
+import com.example.pril.R;
+import com.example.pril.Sett.Settings;
+
+import java.io.File;
+
+public class Music_level_service_5 extends Service {
+    private static final String TAG = "MyService";
+    private MediaPlayer player;
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+
+        player = MediaPlayer.create(this, R.raw.mus_1_5);
+        player.setLooping(true); // зацикливаем
+    }
+
+    @Override
+    public void onDestroy() {
+
+        player.stop();
+    }
+
+    @Override
+    public void onStart(Intent intent, int startid) {
+
+        player.start();
+    }
+}
